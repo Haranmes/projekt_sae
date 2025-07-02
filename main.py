5324b208b72a163060c17dddd5b0caa77db4ad9a
timmay <94752990+Timmaayyyy@users.noreply.github.com>
2025-07-02 18:03:27 +0000

    fix: classes are now json serializeable due '__dict__' parameter chore: outcommented code i dont need right now

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ def main():
         zeitschrift = Zeitschrift("Tim Gotschalk", "12", 2023)
         digitales_medium = DigitalesMedium("eine undendliche geschichte", "CD", "2:00")
 
-        verwaltung.medium_hinzufuegen(buch.to_json())
-        verwaltung.medium_hinzufuegen(zeitschrift.to_json())
-        verwaltung.medium_hinzufuegen(digitales_medium.to_json())
-
-        nutzer = Nutzer("1", "Max Mustermann")
+        verwaltung.medium_hinzufuegen(buch.__dict__)
+        verwaltung.medium_hinzufuegen(zeitschrift.__dict__)
+        verwaltung.medium_hinzufuegen(digitales_medium.__dict__)
         
-        verwaltung.nutzer_hinzufuegen(nutzer)
-        verwaltung.ausleihen("1", "1")
+        # nutzer = Nutzer("1", "Max Mustermann")
+        
+        # verwaltung.nutzer_hinzufuegen(nutzer)
+        # verwaltung.ausleihen("1", "1")
         
     except ValueError as e:
         print(f"Fehler: {e}")
