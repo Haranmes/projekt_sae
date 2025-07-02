226f3163f89c1e97231ab8abc652a0e48ce4b33e
timmay <94752990+Timmaayyyy@users.noreply.github.com>
2025-07-02 18:04:37 +0000

    new: file 'nutzer.py' that holds the 'Nutzer' class

--- Medienarten/nutzer.py
+++ Medienarten/nutzer.py
@@ -1,13 +1,7 @@
 class Nutzer():
-    def __init__(self, id: int, name: str):
+    def __init__(self, id: str, name: str):
         self.__id = id
         self.__name = name
         self.__ausgeliehene_medien = []
-    def to_dict(self):
-        return {
-            'id': self.__id,
-            'name': self.__name,
-            'ausgeliehene_medien': self.__ausgeliehene_medien
-        }
     
     
