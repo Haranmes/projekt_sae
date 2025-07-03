ecc081611a2aa6aa209f159dda73466bd457fa02
Tim herb <144007810+TimHerb2005@users.noreply.github.com>
2025-07-03 15:34:17 +0000

    feat: to_dict method to convert class into a dict

--- Medienarten/digitalesMedium.py
+++ Medienarten/digitalesMedium.py
@@ -3,5 +3,15 @@ from .medium import Medium
 class DigitalesMedium(Medium):
     def __init__(self, titel: str, format: str, laufzeit: str):
         super().__init__(titel)
-        self.__format = format
-        self.__laufzeit = laufzeit
+        self._format = format
+        self._laufzeit = laufzeit
+    
+    def to_dict(self):
+        """Gibt ein Dictionary mit sauberen Attributnamen zurück"""
+        return {
+            'id': self._id,
+            'titel': self._titel,
+            'format': self._format,
+            'laufzeit': self._laufzeit,
+            'ausgeliehen': self._ausgeliehen
+        }
