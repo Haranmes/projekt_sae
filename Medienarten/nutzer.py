373d100dbcc06b6f6dbbd4b57fed4e29960e35d8
Tim herb <144007810+TimHerb2005@users.noreply.github.com>
2025-07-03 15:35:46 +0000

    feat: added to_dict method for dict converting

--- Medienarten/nutzer.py
+++ Medienarten/nutzer.py
@@ -3,5 +3,11 @@ class Nutzer():
         self.__id = id
         self.__name = name
         self.__ausgeliehene_medien = []
+    def to_dict(self):
+        return {
+            'id': self.__id,
+            'name': self.__name,
+            'ausgeliehene_medien': self.__ausgeliehene_medien
+        }
     
     
